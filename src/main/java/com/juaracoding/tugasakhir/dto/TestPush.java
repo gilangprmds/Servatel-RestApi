0dc708e43f026d54d5e88087b77037bec74ec1a8
triandika98 <febbytriandika>
2025-02-15 20:24:17 +0700

    commit test push

---
+++ src/main/java/com/juaracoding/tugasakhir/dto/TestPush.java
@@ -0,0 +1,15 @@
+package com.juaracoding.tugasakhir.dto;
+
+/*
+Created By IntelliJ IDEA 2024.3 (Community Edition)
+Build #IC-243.21565.193, built on November 13, 2024
+@Author USER Febby Tri Andika
+Java Developer
+Created on 15/02/2025 20:22
+@Last Modified 15/02/2025 20:22
+Version 1.0
+*/
+public class TestPush {
+
+    private String name;
+}
