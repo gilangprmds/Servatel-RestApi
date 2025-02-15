915a8cce45fca9fee0f1998ef61e1646fd982579
gilangprmds <gilangpramudias@gmail.com>
2025-02-15 20:17:58 +0700

    resproomdto

---
+++ src/main/java/com/juaracoding/tugasakhir/dto/respone/RespRoomDTO.java
@@ -0,0 +1,18 @@
+package com.juaracoding.tugasakhir.dto.respone;
+
+import com.juaracoding.tugasakhir.enums.RoomType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RespRoomDTO {
+    private Long id;
+    private RoomType roomType;
+    private Integer roomCount;
+    private Double pricePerNight;
+    private Integer maxAvailableRooms;
+    private Integer maxGuest;
+}
